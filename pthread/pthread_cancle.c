a0ce15b29ed0172a4a620f01bfdde9d2612ca36e
wangyilongln <wyl2005@163.com>
2019-08-23 11:56:06 +0800

    update.

---
+++ pthread/pthread_cancle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *thread_fun(void *arg)
+{
+    int i = 1;
+    printf("thread start!\n");
+
+    while(1)
+    {
+        i++;
+        pthread_testcancel();
+    }
+
+    return (void *)0;
+}
+
+int main()
+{
+    void *ret = NULL;
+    int iret = 0;
+
+    pthread_t tid;
+
+    pthread_create(&tid, NULL, thread_fun, NULL);
+    sleep(1);
+    pthread_cancel(tid);
+    pthread_join(tid, &ret);
+    
+    printf("thread 3 exit code %d \n", (int)ret);
+
+    return 0;
+}
